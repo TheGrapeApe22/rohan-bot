ddf1d7c22f82f6ddcaf0391f27438b1453773339
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-09-15 21:52:11 -0700

    minor touches

--- cogs/link_previews.py
+++ cogs/link_previews.py
@@ -14,7 +14,7 @@ class LinkPreviews(commands.Cog):
         self.bot = bot
 
     @commands.hybrid_command(help="generate a funny website preview with a different redirect")
-    async def breaking_news(self, ctx, title: str=None, description: str=None, image_url: str=None, fake_url: str=None, provider: str=None, author: str=None, large_image: bool=True, template: Literal['NYTimes', 'AP News', 'Prospector', 'BBC', 'None']='None'):
+    async def breaking_news(self, ctx, title: str=None, description: str=None, image_url: str=None, message_text: str=None, provider: str=None, author: str=None, large_image: bool=True, template: Literal['NYTimes', 'AP News', 'Prospector', 'BBC', 'None']='None'):
         destination_url = 'https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html' # no making this a parameter, because abusable
 
         def cleaned(value: str | None) -> str:
@@ -29,28 +29,30 @@ class LinkPreviews(commands.Cog):
 
         if template == 'NYTimes':
             yesterday = date.today() - timedelta(days=1)
-            fake_url = fake_url or f'https://www.nytimes.com/{yesterday.strftime("%Y/%m/%d")}/politics/{title_in_url}.html'
+            message_text = message_text or f'https://www.nytimes.com/{yesterday.strftime("%Y/%m/%d")}/politics/{title_in_url}.html'
             provider = provider or 'The New York Times'
             author = author or 'By Mike Isaac'
             image_url = image_url or 'https://static01.nyt.com/newsgraphics/images/icons/defaultPromoCrop.png'
             large_image = True
         elif template == 'AP News':
-            fake_url = fake_url or f'https://apnews.com/article/{title_in_url}-d2d1bac8e8666c681937665596a4f603'
+            message_text = message_text or f'https://apnews.com/article/{title_in_url}-d2d1bac8e8666c681937665596a4f603'
             provider = provider or 'AP News'
             author = author or 'World News'
             image_url = image_url or 'https://static01.nyt.com/newsgraphics/images/icons/defaultPromoCrop.png'
             large_image = False
         elif template == 'BBC':
-            fake_url = fake_url or f'https://www.bbc.com/news/articles/ce8767g4jdpo'
+            message_text = message_text or f'https://www.bbc.com/news/articles/ce8767g4jdpo'
             provider = provider or 'BBC News'
             author = author or 'By Mark Elliot'
             image_url = image_url or 'https://static.wikia.nocookie.net/logopedia/images/b/ba/BBC_News_2019_%28Black_box%29.svg/revision/latest/scale-to-width-down/250?cb=20211024233853'
             large_image = False
         elif template == 'Prospector':
-            fake_url = fake_url or f'https://prospector.com/11608/news/{title_in_url}'
+            message_text = message_text or f'https://prospector.com/11608/news/{title_in_url}'
             provider = provider or 'The Prospector'
             image_url = image_url or 'https://chsprospector.com/wp-content/uploads/2025/08/prospector-masthead-enhanced.png'
             large_image = True
+        else:
+            message_text = message_text or "cheese"
 
         first = True
         def conj():
@@ -74,17 +76,18 @@ class LinkPreviews(commands.Cog):
             previewed_url += f'{conj()}author_name={cleaned(author)}'
         previewed_url += f'{conj()}author_url={cleaned(destination_url)}'
         previewed_url += f'{conj()}provider_url={cleaned(destination_url)}'
-        previewed_url += f'{conj()}appear_url={cleaned(fake_url or "https://www.example.com")}'
+        previewed_url += f'{conj()}appear_url={cleaned(message_text)}'
         previewed_url += f'{conj()}large_image={str(large_image).lower()}'
 
         out = ''
-        if '//' in fake_url:
-            sep = fake_url.find('//') + 2
-            out += f'[{fake_url[:sep]}](<{destination_url}>)'
+        if '//' in message_text:
+            sep = message_text.find('//') + 2
+            out += f'[{message_text[:sep]}](<{destination_url}>)'
         else:
             sep = 0
-        out += f'[{fake_url[sep:-1]}](<{destination_url}>)'
-        out += f'[{fake_url[-1]}]({previewed_url})'
+        if len(message_text) > 1:
+            out += f'[{message_text[sep:-1]}](<{destination_url}>)'
+        out += f'[{message_text[-1]}]({previewed_url})'
 
         if len(out) > 2000:
             await ctx.send(f"Message ({len(out)} characters) too long to send.")
@@ -102,7 +105,7 @@ class LinkPreviews(commands.Cog):
                     title=res['title'],
                     description=res['description'],
                     image_url=res['image'],
-                    fake_url=link,
+                    message_text=link,
                     provider=res['provider_name'] or res['site_name'],
                     author=res['author_name'],
                     large_image=res['card'] == 'summary_large_image',
